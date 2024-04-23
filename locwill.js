7f84494d87dec5f7aa7756dc07f98bbbc1b42c03
SCMariners <sc.mariners@skynet.be>
2024-04-23 10:16:53 +0200

    Upd_23042024

--- locwill.js
+++ locwill.js
@@ -155,3 +155,5 @@ calreservation.push(202404132100);
 calreservation.push(202404152200);
 calreservation.push(202404242000);
 calreservation.push(202405222200);
+calreservation.push(202405272100);
+calreservation.push(202405272200);
